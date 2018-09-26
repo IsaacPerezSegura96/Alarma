e49e61626bec3751492e9358d68cc327a196f081
IsaacPerezSegura96 <Isaac.Perez96@hotmail.com>
2018-09-26 18:39:30 -0500

    Declaracion de variables y libreria
    Se han definido las variables a utilizar para controlar el rebote del switch, ademas de incluir la librerias que nos permitira hacer un sleep.

---
+++ Alarma.ino
@@ -0,0 +1,22 @@
+/* Incluyendo la libreria */
+#include "LowPower.h"
+
+#define reedSwitch 2
+#define buzzer 3
+
+// Variables para eliminar el rebote del switch
+long tiempo = 0;
+
+void setup() {
+  
+
+}
+
+void loop() {
+
+}
+
+// Metodo encargado de eliminar el rebote y denota el "wake up" del arduino
+void wakeUp(){
+  
+}
