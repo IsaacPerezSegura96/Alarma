ed5f926bd762061fff905f54a48e9ed13a4ddbaa
IsaacPerezSegura96 <Isaac.Perez96@hotmail.com>
2018-09-26 18:59:29 -0500

    Construyendo
    Se realizo cambio de pin para el buzzer, se agregaron comentarios y un while que me permitira activar el buzzer independientemente

--- Alarma.ino
+++ Alarma.ino
@@ -2,7 +2,7 @@
 #include "LowPower.h"
 
 #define reedSwitch 2
-#define buzzer 3
+#define buzzer 4
 
 // Variables para eliminar el rebote del switch
 long tiempo = 0;
@@ -13,16 +13,19 @@ void setup() {
 }
 
 void loop() {
-// Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(0, wakeUp, LOW);
-    
-    // Enter power down state with ADC and BOD module disabled.
-    // Wake up when wake up pin is low.
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-    
-    // Disable external pin interrupt on wake up pin.
-    detachInterrupt(0);
-digitalWrite(buzzer,HIGH);
+  // Creacion de la interrupcion que nos permitira salir del SLEEP mediante PULL UP
+  attachInterrupt(0, wakeUp, LOW);
+  // Se realiza la reduccion de energia hasta que se detecte un LOW
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  //Se deshabilita la interrupcion para poder implementar acciones al buzzer
+  detachInterrupt(0);
+  /* <--- Mis acciones --->*/
+  // Mientras se active el switch en su modalidad PULL UP (es decir que se detecte LOW) se accionara la alarma
+  while(digitalRead(reedSwitch)==LOW){
+    digitalWrite(buzzer,HIGH);
+  }
+  // Se desactiva la alarma cuando la puerta se cierra
+  digitalWrite(buzzer,LOW);
 }
 
 // Metodo encargado de eliminar el rebote y denota el "wake up" del arduino
